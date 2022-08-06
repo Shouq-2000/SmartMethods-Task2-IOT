364f40d08e74bf50db4b7e8374df7196f97ff58c
Shouq-2000 <107880084+Shouq-2000@users.noreply.github.com>
2022-08-06 23:03:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+var SpeechRecognition = window.webkitSpeechRecognition
+ var recognition = new SpeechRecognition()
+ var textbox = $("#textbox")
+ var instructions = $("#instr")
+ var content = ""
+
+ recognition.continuous = true
+
+ recognition.onstart = function(){
+    instructions.text('Voice Recognition is on')
+ }
+recognition.onspeechend = function () {
+    instructions.text('No Activity')
+}
+ 
+recognition.onerror = function (){
+    instructions.text('Try Again')
+
+}
+
+recognition.onresult = function (event) {
+    var current = event.resultIndex 
+
+   var transcript = event.results[current][0].transcript
+
+   content += transcript
+
+   textbox.val(content)
+}
+
+ $("#s-btn").click(function (event) {
+    if(content.length) {
+        content += ''
+
+    }
+    recognition.start()
+  
+
+ })
+
+ textbox.on('input',function() {
+    content = $(this).val()
+    
+    getCommand();
+ })
